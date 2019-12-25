1850b4c694fd2e8f5742f472590e796ef07a4616
xuergo <859204948@qq.com>
2019-12-25 17:29:27 +0800

    成功实现GitHub登录

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ##社区论坛##
-[giao](https://baidu.com)
+- [giao](https://baidu.com)
+- [github 回调地址](http://localhost:8887/callback)
---
+++ src/main/java/com/xuxiang/community/controller/AuthorizeController.java
@@ -0,0 +1,51 @@
+package com.xuxiang.community.controller;
+
+import com.xuxiang.community.dto.AccessTokenDTO;
+import com.xuxiang.community.dto.GithubUser;
+import com.xuxiang.community.procider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletResponse response) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        return "index";
+    }
+
+
+}
